5cd015a2e5b4d12897007ab93e1a7aa89eb2edf3
Eric Anastas <aireq303@gmail.com>
2015-12-20 21:09:46 -0800

    Completed Exercise 9

--- Exercise9.js
+++ Exercise9.js
@@ -1,10 +1,7 @@
-//This is just a test of SourceTree on my laptop
-//This is just a test of SourceTree on my other laptop
-
 var http = require("http");
 var bl = require('bl');
 
-
+//Reads input URls
 var urls = [];
 urls[0] = process.argv[2];
 urls[1] = process.argv[3];
@@ -12,33 +9,66 @@ urls[2] = process.argv[4];
 
 var results = [];
 
+//console.log("urls[0]="+urls[0]);
+//console.log("urls[1]="+urls[1]);
+//console.log("urls[2]="+urls[2]);
+
 
 var returned = 0;
 
-for(var i=0; i<urls.length;i++){
+//console.log("starting for loop...")
+for (var i = 0; i < urls.length; i++) {
 	
-	var url = urls[i];
-		
-	http.get(url,function(response){
+	//console.log("calling httpGet("+i.toString()+")...")
+	httpGet(i);
+}
+
+
+function httpGet(index) {
+
+	var url = urls[index];
 	
-		response.pipe(bl(function(err,data){
-			if(err) console.error('There was an error!',err);
+	
+	//console.log("httpGet("+index.toString()+")..started. url = "+url);
+
+	http.get(url, function (response) {
+		response.pipe(bl(function (err, data) {
+
+			if (err) console.error('There was an error!', err);
+			
+			
 			var dataStr = data.toString();
+			results[index] = dataStr;
+			returned++;
 			
+			//console.log("dataStr.length = "+dataStr.length);
+			//console.log("returned = "+returned.toString());
 			
-			results[i] = dataStr;
-			
-			if(results.length = results.length){
-				for(var j=0; i<results.length;i++){
-					console.log(results[j]);
-				}	
+			if(returned == 3)
+			{
+				//console.log("Recorded 3 results, printing..")
+				
+				for(var resIdx in results)
+				{
+					console.log(results[resIdx]);
+				}
+				
+				
+			}
+			else
+			{
+				//console.log("...not 3 results yet")
+				
 			}
 		}
-		)); //End of response.pipe()		
-		
-			
+			)); //End of response.pipe()		
 	}); //End of http.get()
 }
+
+
+
+
+
 
 
 
