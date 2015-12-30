79c24ec3297a86aab2f5b873e3ee118dac7fdbd8
Eric Anastas <eric.anastas@som.com>
2015-12-29 19:06:23 -0800

    Working on stream adventure exercise 11

--- stream-adventure/Exercise11.js
+++ stream-adventure/Exercise11.js
@@ -1,7 +1,9 @@
 var spawn = require('child_process').spawn;
-var duplex = require('duplexer2');
+var duplex = require('duplexer');
 
 module.exports = function(cmd, args){
 	var childProc = spawn(cmd, args);
 	return duplex(childProc.stdin, childProc.stdout);	
 };
+
+//http://stackoverflow.com/questions/23387960/how-to-solve-nodeschools-duplexer-challange-without-the-duplexer-package
